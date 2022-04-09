df0988650b55968096eda61fd5433842c09bebf2
JijiKusahara <66423096+JijiKusahara@users.noreply.github.com>
2022-04-09 21:57:16 +0900

    state-model
    stateの初期化
    クラスの基礎

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import defaultDataset from './dataset';
+import './assets/styles/style.css'
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ansewrs: [],
+            chats: [],
+            currentId: "init",
+            datasset: defaultDataset,
+            open: false
+        }
+    }
+
+    render() {
+      return (
+        <section className='c-section'>
+          <div className='c-box'>
+              {this.state.currentId} 
+          </div>
+        </section>
+      );
+    }
+}
+
